799a68888fed05e66fd905a6a609ad0e0a38153e
nnutica <nitinat.loru@bumail.net>
2025-05-16 04:27:00 +0700

    Remove DiaryDetailPage and refactor DiaryHistoryPage layout and logic

--- Views/DiaryHistoryPage.xaml.cs
+++ Views/DiaryHistoryPage.xaml.cs
@@ -1,34 +1,26 @@
 using Righthere_Demo.Data;
 using Righthere_Demo.Models;
 
-
 namespace Righthere_Demo.Views;
 
 public partial class DiaryHistoryPage : ContentPage
 {
 	private DiaryDatabase _diaryDb = new DiaryDatabase();
+
 	public DiaryHistoryPage()
 	{
-
 		InitializeComponent();
-		_diaryDb = new DiaryDatabase();
-
 	}
+
 	protected override async void OnAppearing()
 	{
 		base.OnAppearing();
 
 		var diaries = await _diaryDb.GetDiariesByUserAsync(App.User.Userid);
-		DiaryListView.ItemsSource = diaries;
+
+		// เรียงลำดับจากวันที่ล่าสุดลงมา
+		DiaryListView.ItemsSource = diaries.OrderByDescending(d => d.CreatedAt).ToList();
 	}
 
 
-	private async void DiaryListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
-	{
-		if (e.CurrentSelection.FirstOrDefault() is DiaryData selectedDiary)
-		{
-			await Navigation.PushAsync(new Views.DiaryDetailPage(selectedDiary));
-			DiaryListView.SelectedItem = null;
-		}
-	}
 }
